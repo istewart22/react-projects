23f889561bcae7d3fdd0ef57552fe675c0344348
Iain Stewart <iainstewart08@gmail.com>
2022-08-12 22:40:40 +0100

    Complete tour app

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, image, info, price, name, removeTour }) => {
+  const [expandedText, setExpendedText] = useState(false);
+
+  return (
+    <article className='single-tour'>
+      <img src={image} alt={name} />
+      <footer>
+        <div className='tour-info'>
+          <h4>{name}</h4>
+          <h4 className='tour-price'>${price}</h4>
+        </div>
+        <p>{expandedText ? `${info.substring(0, 200)}...` : info}</p>
+        <button onClick={() => setExpendedText(!expandedText)}>
+          {expandedText ? 'Read more' : 'Show less'}
+        </button>
+        <button className='delete-btn' onClick={() => removeTour(id)}>
+          Not interested
+        </button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
