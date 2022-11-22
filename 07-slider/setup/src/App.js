852d71837bb1b40f60a6a4a0816e64405bbb8b6b
Iain Stewart <iainstewart08@gmail.com>
2022-11-22 23:42:46 +0000

    Complete project 7

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,9 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
+import Review from './Review';
+
 function App() {
-  return <h2>slider project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  const personData = people[index];
+
+  const handlePrevClick = () => {
+    if (index > 0 && index <= data.length - 1) {
+      setIndex(index - 1);
+    } else {
+      setIndex(data.length - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (index >= 0 && index < data.length - 1) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  };
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      handleNextClick();
+    }, 2000);
+
+    return () => clearInterval(slider);
+  }, [index]);
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className='section-center'>
+        <Review {...personData} />
+        <button className='prev' onClick={handlePrevClick}>
+          <FiChevronLeft />
+        </button>
+        <button className='next' onClick={handleNextClick}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
---
+++ 07-slider/setup/src/Review.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FaQuoteRight } from 'react-icons/fa';
+
+function Review({ id, image, name, title, quote }) {
+  return (
+    <article key={id}>
+      <img src={image} alt={name} className='person-img' />
+      <h4>{name}</h4>
+      <p className='title'>{title}</p>
+      <p className='text'>{quote}</p>
+      <FaQuoteRight className='icon' />
+    </article>
+  );
+}
+
+export default Review;
